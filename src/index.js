a5e61c1dec3b91b4f6306b8eccb083651d40a88a
Eileen Bednar <bednar.eileen@web.de>
2019-10-17 17:43:54 +0200

    1. Vordefinierte index.js 2. Eigene Versuche gemacht, um die Buttons zu disabeln bzw. enablen

--- src/index.js
+++ src/index.js
@@ -7,3 +7,40 @@ window.addEventListener("load" , () => {
     let app = new App();
     app.start();
 });
+
+/*providing Upgrades, after quantity of clicks*/
+let clicks;
+
+
+/*providing Upgrades, after quantity of clicks for Buckets*/
+let requiredClicksForBucket;
+
+if (clicks >= requiredClicksForBucket)
+{
+    document.getElementById('button').style.disabled="true";
+}
+
+if(clicks < requiredClicksForBucket)
+{
+    document.getElementById('button').style.disabled="false";
+}
+
+/*providing Upgrades, after quantity of clicks for IdeonellaSakariensis*/
+let requiredClicksForIdeonellaSakariensis;
+
+if (clicks >= requiredClicksForIdeonellaSakariensis)
+{
+    document.getElementById('button').style.disabled="true";
+}
+if(clicks < requiredClicksForIdeonellaSakariensis)
+{
+    document.getElementById('button').style.disabled="false";
+}
+
+
+
+
+
+
+
+
