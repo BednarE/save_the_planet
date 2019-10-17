8552127c9b7c8315abde7a5ec1d38f315bc97e9a
florian <florian4774@gmail.com>
2019-10-17 17:25:10 +0200

    added showProducts() which creates a list & view of all products

--- src/workshop/workshop.js
+++ src/workshop/workshop.js
@@ -1,9 +1,42 @@
 "use strict";
 
+import Product from "./Product";
+
 class Workshop {
 
     constructor() {
-        document.getElementById("content").innerHTML=document.getElementById("workshopsite").innerText;
+        document.getElementById("title").innerText = "Workshop";
+
+        this._products = [
+            new Product("Plastikring", "Ein nicht-so glänzender Plastikring", 2, 10, null),
+            new Product("Plastik Kette", "sehr leicht und bröselig", 3, 15, null)
+        ];
+
+        this.showProducts();
+    }
+
+    showProducts() {
+        let productTemplate = document.getElementById("productTemplate");
+        let productList = [];
+        for (let product of this._products) {
+            let newProduct = document.createElement("div");
+            newProduct.innerHTML = productTemplate.innerHTML;
+            newProduct.getElementsByClassName("productName")[0].innerText = product._name;
+            newProduct.getElementsByClassName("description")[0].innerText = product._description;
+            newProduct.getElementsByClassName("plasticcost")[0].innerText = "Kostet: " + product._plasticCost + " Plastik";
+            newProduct.getElementsByClassName("productMoneyValue")[0].innerText = "Wert: " + product._moneyValue + "€";
+            newProduct.getElementsByClassName("buyProductButton")[0].id = product._name; //Set the id of the button
+            productList.push(newProduct.innerHTML);
+            document.getElementById("content").appendChild(newProduct);
+            //You can only add the Event Listeners after the element has been added to the DOM!
+            newProduct.getElementsByClassName("buyProductButton")[0].addEventListener("click", (product) => {
+                this.buyProduct(product);
+            });
+        }
+    }
+
+    buyProduct(event) {
+        console.log("You have bought a " + event.target.id);
     }
 
 }
