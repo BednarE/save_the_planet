2bf4b4ef2e95f16ac332e9e5cbf02a7b94766fd3
Selami Dagtasoglu <sdagtasoglu@gmail.com>
2019-10-18 17:17:51 +0200

    Clicker button wurde hinzugefügt.

--- src/clicker/clicker.js
+++ src/clicker/clicker.js
@@ -5,6 +5,11 @@ class Clicker {
     constructor() {
         document.getElementById("title").innerText = "Main Page";
     }
+    var clicks = 0;
+    function incrementClick() {
+        clicks += 1;
+        document.getElementById("clicks").innerHTML = clicks;
+    };
 
 }
 
