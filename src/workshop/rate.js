7665eec6bfe6b991f106d6b1ec18f398238527dd
Kevin Hink <kevin.hink99mail.com>
2019-10-18 16:22:13 +0200

    getCurrentRate and update implemented

---
+++ src/workshop/rate.js
@@ -0,0 +1,25 @@
+"use strict";
+
+class Rate {
+
+    constructor(used_amount, updateInterval, baseRate, currentRate){
+        this.used_amount=used_amount;
+        this.updateInterval=updateInterval;
+        this.baseRate=baseRate;
+        this.currentRate=currentRate;
+    }
+
+    getCurrentRate(){
+        if(){ // Timesclicked > 100
+            this.currentRate=this.baseRate*((Math.random()+1)*10);
+        }
+        else{
+            this.currentRate=this.baseRate*(Math.random()*10);
+        }
+        return this.currentRate;
+    }
+
+    update(){
+        this.getCurrentRate();
+    }
+}
