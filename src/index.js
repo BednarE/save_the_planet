b94cd2c6ef99dd61dfc49dd0207745f0caf70563
Eileen Bednar <bednar.eileen@web.de>
2019-10-19 10:22:42 +0200

    1. Hinzufügen einer Funktion bei "window.onload", dass Javascript erst geladen wird, wenn HTML fertig geladen wurde. 2. Erstellung der Variable test1 (zu Testzwecken), die auf den Button test1 des HTMLs zugreift. 3. Erstellung der Variablen "clicks" und "cookieclicker" zu Testzwecken 4. Kontrolle der Ausgabe mit console.log 5. Variable "clicks" unterschiedliche Werte zugewiesen um die folgende Funktion zu testen. Test war erfolreich. 6. Funktion checkBucket erstellt. Als Paramter wird "clicks" mitgegeben. 	Wenn die Clicks größer sind als 100 (eig. requiredClicks, zu Testzwecken hier mit Werten z.B. 100) 	Testbutton wird enabled 	Falls die Clicks kleiner als 100 sind und gleichzeitig der Button schon einmal enabled wurde, wird der Button nur disabled 	In allen anderen Fällen soll zukünftig das Fragezeichen-Bild angezeigt werden 7. Eine neue Funktion erstellt, die in einem Intervall von einer 1/10 Sekunde alle Funktionen für jedes einzelne Upgrade durchführt 	Zu Testzwecken wird hier nur mit checkBucket(clicks) gearbeitet

--- src/index.js
+++ src/index.js
@@ -1,21 +1,87 @@
 "use strict";
 
-import App from "./app";
+//import App from "./app";
+<!---Mainside: Zählbuttons Upgrades-->
 
-window.addEventListener("load" , () => {
+window.onload = function() {
+    let test1 = document.getElementById("test1");
+    test1.setAttribute("disabled", "disabled");
+    let bBucket = document.getElementsByTagName("bBucket");
+    /*let bVacuumCleaner = document.getElementById(elementId: "bVacuumCleaner");
+    let bIdeonella = document.getElementById(elementId: "bIdeonella");
+    let bDrone = document.getElementById(elementId: "bDrone");
+    let bDipNet= document.getElementById(elementId: "bDipNet");
+    let bMagnetic = document.getElementById(elementId: "bMagnetic");
+    let bCat = document.getElementById(elementId: "bCat");
+    let bNet2 = document.getElementById(elementId: "bNet2");
+    let bTimeMachine = document.getElementById(elementId: "bTimeMachine");
+    let bBlackhole = document.getElementById(elementId: "bBlackhole"); */
+    let clicks;
+    let cookieclicker;
+    console.log(bBucket);
+    console.log(test1);
+    bBucket.disabled=false;
+    console.log(bBucket.disabled);
+
+    clicks = 10;
+
+    /*document.addEventListener("DOMContentLoaded", function () {
+        document.querySelector('[name="button"]').addEventListener('change', changeStyle);
+        document.querySelector('[name="background"]').addEventListener('change', changeStyle);
+
+        function changeStyle() {
+            let output = document.querySelector('output');
+            output.style.color = document.querySelector('[name="test1"]').value;
+            output.style.backgroundColor = document.querySelector('[name="background"]').value;
+            document.getElementById("test1").style.color = "#585858";
+        }
+    });*/
+    function checkBucket(clicks)
+    {
+
+
+        if(100 <= clicks)
+        {
+            test1.removeAttribute("disabled")
+            /*Button enablen*/
+            bBucket.disabled = false;
+
+        }
+        else if(100 > clicks /*&& bBucket.disabled === false*/)
+        {
+            test1.setAttribute("disabled", "disabled");
+            /*Hintergrundbild ausgrauen und disablend*/
+            //bBucket.disabled = true;
+        }
+        else
+        {
+            /*Grundbild lassen*/
+        }
+
+    }
+    window.setInterval(function() {
+        checkBucket(clicks);
+
+    },100);
+
+};
+
+
+/*window.addEventListener("load" , () => {
     //Anwendung starten
     let app = new App();
     app.start();
-});
+});*/
 
 /*providing Upgrades, after quantity of clicks*/
-let clicks;
+
 
 
 /*providing Upgrades, after quantity of clicks for Buckets*/
-let requiredClicksForBucket;
 
+/*
 if (clicks >= requiredClicksForBucket)
+
 {
     document.getElementById('button').style.disabled="true";
 }
@@ -26,6 +92,7 @@ if(clicks < requiredClicksForBucket)
 }
 
 /*providing Upgrades, after quantity of clicks for IdeonellaSakariensis*/
+/*
 let requiredClicksForIdeonellaSakariensis;
 
 if (clicks >= requiredClicksForIdeonellaSakariensis)
@@ -36,6 +103,11 @@ if(clicks < requiredClicksForIdeonellaSakariensis)
 {
     document.getElementById('button').style.disabled="false";
 }
+
+*/
+
+
+
 
 
 
