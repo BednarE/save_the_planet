8a78a3b93b73683e339d679cd3b3f52638620d4e
Eileen Bednar <bednar.eileen@web.de>
2019-10-25 21:24:38 +0200

    Hinzufügen der Klasse Collector Erstellung eines Konstrukturs mit den Paramtern name, picture, requiredMoney, id

---
+++ src/clicker/collector.js
@@ -0,0 +1,15 @@
+"use strict";
+
+class Collector {
+
+    constructor(name, picture, requiredMoney, id) {
+        this.name = name;
+        this.picture = picture;
+        this.requiredMoney = requiredMoney;
+        this.id = id;
+        this.unlocked = false;
+    }
+
+
+}
+export default Collector;
