2931f29d555842d3ced244a1891aaf8b917111e0
Kevin Hink <kevin.hink99mail.com>
2019-10-27 11:38:53 +0100

    added methods for buying a product and for disabling buttons

--- src/workshop/workshop.js
+++ src/workshop/workshop.js
@@ -27,12 +27,12 @@ class Workshop {
             newProduct.getElementsByClassName("description")[0].innerText = product._description;
             newProduct.getElementsByClassName("plasticcost")[0].innerText = "Kostet: " + product._plasticCost + " Plastik";
             newProduct.getElementsByClassName("productMoneyValue")[0].innerText = "Wert: " + product._moneyValue + "€";
-            newProduct.getElementsByClassName("productionTime")[0].innerText = "Produktionszeit: " + (product._productionTime /1000) + "s";
+            newProduct.getElementsByClassName("productionTime")[0].innerText = "Produktionszeit: " + (product._productionTime / 1000) + "s";
             newProduct.id = product._name; //Set the id of the button
             document.getElementById("workshopTemplate").appendChild(newProduct);
             //You can only add the Event Listeners after the element has been added to the DOM!
             newProduct.getElementsByClassName("buyProductButton")[0].addEventListener("click", (mouseEvent) => {
-                this.constructProduct(product);
+                this.constructProduct(product); //needs to be buyingProduct() later with the checking if enough plastic is given
             });
 
             //Check if the product is currently under construction
@@ -65,10 +65,12 @@ class Workshop {
             product.setCurrentlyUnderConstruction(true);
             //Add the interval to the window object, so it will continue to run even if the Workshop class has been unloaded.
             //We need to add it with a different name for each product, so different products wil not overwrite each other
-            window["constructionInterval_"+product._name] = setInterval(function () {
+            window["constructionInterval_" + product._name] = setInterval(function () {
                 let seconds = Math.floor((waitTime / 1000)); //TODO: Fix the display time
                 let minutes = Math.floor((waitTime / (1000 * 60)));
                 let hours = Math.floor((waitTime / (1000 * 60 * 60)));
+                seconds = seconds - (minutes * 60);
+                minutes = minutes - (hours * 60);
                 //Store the productionTime inside the product, so we can display it right away once the subpage has been switched to another page
                 //and back
                 product.setLeftConstructionTime(hours, minutes, seconds);
@@ -77,15 +79,15 @@ class Workshop {
                 if (productHtml != null) {
                     productHtml.getElementsByClassName("productionTimeLeft")[0].innerHTML =
                         "Wird hergestellt, übrige Zeit: " + hours + "h " + minutes + "m " + seconds + "s ";
-                    let percent = Math.round((1 -(waitTime / product._productionTime)) * 100);
-                    productHtml.getElementsByClassName("progress-bar")[0].setAttribute("aria-valuenow", ""+percent);
+                    let percent = Math.round((1 - (waitTime / product._productionTime)) * 100);
+                    productHtml.getElementsByClassName("progress-bar")[0].setAttribute("aria-valuenow", "" + percent);
                     productHtml.getElementsByClassName("progress-bar")[0].style = "width: " + percent + "%;";
                     productHtml.getElementsByClassName("progress-bar")[0].innerHTML = percent + "%";
                 }
 
                 //Construction has been finished
                 if (waitTime <= 0) {
-                    clearInterval(window["constructionInterval_"+product._name]);
+                    clearInterval(window["constructionInterval_" + product._name]);
                     product.setCurrentlyUnderConstruction(false);
                     product.setLeftConstructionTime(0, 0, 0);
                     if (productHtml != null) {
@@ -104,6 +106,30 @@ class Workshop {
             console.error("The product is still under construction!", product);
         }
     }
+
+    buyingProduct(product) {
+        //Check if enough plastic to buy a product then reduce the plastic
+        if (this._game.getPlastic() >= product._plasticCost) {
+            this._game.setPlastic(this._game.getPlastic() - product._plasticCost);
+            // now it can be constucted
+            this.constructProduct(product);
+            // after construction you get the money
+            this._game.setMoney(this._game.getMoney() + product._moneyValue);
+        }//else {  //needs to be imported to make it work, when the buttons are disabled this will be unnecessary
+        //  Swal.fire("Not enough Plastic", "Product can't be bought. You have not not enough plastic","error");
+        //}
+    }
+
+    plasticEnoughForBuying() { // for disabling buttons while checking every second if the button needs to be disabled
+        setInterval(function () {
+            for (let products of this._products) {
+                if (this._game.buttonDisableForPlastic(products._plasticCost)) {
+                    document.getElementById(products._name).getElementsByClassName("buyProductButton")[0].setAttribute("disabled", "disabled");
+                }
+            }
+        }, 1000);
+    }
 }
+
 
 export default Workshop;
