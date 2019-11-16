44bf981bef79303ddfa1ec9214d294f1cf0ced2f
Kevin Hink <kevin.hink99mail.com>
2019-11-16 17:44:47 +0100

    round display for plastic per second on switching sites

--- src/clicker/clicker.js
+++ src/clicker/clicker.js
@@ -42,9 +42,9 @@ class Clicker {
         /*Neuer EventListener wird hinzugefügt, document = gesamte Webseite, getElementbyI sucht den PlasticBall raus (steht in der Clicker
         html, addEventListener mit dem Eventtyp "click" also wenn es geklickt wurde, Methode wird mitgegeben, die den Clicker um 1 erhöht
          */
-        document.getElementById("plasticDisplay").innerHTML = this._game.getPlastic();
+        document.getElementById("plasticDisplay").innerHTML = Math.round(this._game.getPlastic())+"";
         document.getElementById("moneyDisplayed").innerHTML = this._game.getMoney()+" €";
-        document.getElementById("plasticPerSecondDisplayed").innerHTML = this._game.getPlasticPerSecond();
+        document.getElementById("plasticPerSecondDisplayed").innerHTML = (Math.round(this._game.getPlasticPerSecond()*10)/10)+"";
 
         document.getElementById("displayGameData").classList.add("col-sm-12");
         document.getElementById("displayGameData").classList.add("col-md-3");
@@ -150,7 +150,7 @@ class Clicker {
             collector.requiredMoney = Math.round(collector.requiredMoney * 1.5);
             document.getElementById(collector.id).getElementsByClassName("collectorCounts")[0].innerText = collector.count;
             document.getElementById(collector.id).getElementsByClassName("collectorRequiredMoney")[0].innerText = collector.requiredMoney;
-            document.getElementById("plasticPerSecondDisplayed").innerHTML = "" + (Math.round(this._game.getPlasticPerSecond() * 100) / 100);
+            document.getElementById("plasticPerSecondDisplayed").innerHTML = "" + (Math.round(this._game.getPlasticPerSecond() * 10) / 10);
             if (this._game.getPlasticPerSecond() > 30) {
 
                 this._game.setPlasticPerClick(Math.round(Math.floor(this._game.getPlasticPerSecond() / 10) + this._game.getPlasticPerClick() / 10));
